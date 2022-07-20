8d946be6974c0a1b7ccb39364c4b9c6fd8f85bbe
Kelly Woldsethgit config --global user.name Kelly <kelly.woldseth@gmail.com>
2022-07-19 17:51:21 -0700

    url updated

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::bucket002398
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://udagram-api-dev22222.us-east-1.elasticbeanstalk.com/
