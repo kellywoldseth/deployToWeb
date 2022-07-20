614602e838033ef04f67207883d9bfc48a845c48
Kelly Woldsethgit config --global user.name Kelly <kelly.woldseth@gmail.com>
2022-07-19 17:52:24 -0700

    deploy update

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
 aws s3 cp --recursive --acl public-read ./www s3://bucket002398/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://bucket010201/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://bucket002398/
